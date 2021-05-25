a4975678b3e2baec4f28c0449064046806ac6681
thenatural86 <philip.smalls86@gmail.com>
2021-05-25 17:38:31 -0400

    grouping test together using testing suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import Greeting from './Greeting'
 
-test('renders Yolo World as a text', () => {
-  // Arrange
-  render(<Greeting />)
+describe('Greeting component', () => {
+  test('renders Yolo World as a text', () => {
+    // Arrange
+    render(<Greeting />)
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const yoloWorldElement = screen.getByText('Yolo World')
-  expect(yoloWorldElement).toBeInTheDocument()
+    // Assert
+    const yoloWorldElement = screen.getByText('Yolo World')
+    expect(yoloWorldElement).toBeInTheDocument()
+  })
 })
