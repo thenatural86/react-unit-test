7bfbe5689ae751af1dcb5f30ad48e941eb27987d
thenatural86 <philip.smalls86@gmail.com>
2021-05-26 10:58:38 -0400

    use async/await keywords

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react'
 import Async from './Async'
 
 describe('Async component', () => {
-  test('renders post if request succeeds', () => {
+  test('renders post if request succeeds', async () => {
     render(<Async />)
 
-    const listItemElements = screen.getAllByRole('listitem')
+    const listItemElements = await screen.findAllByRole('listitem')
     expect(listItemElements).not.toHaveLength(0)
   })
 })
